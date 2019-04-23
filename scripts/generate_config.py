be8a434ee8338e6ceda427fc5b7dffe4b6d03e9e
Yossi Solomon <yosisolomon@gmail.com>
2019-04-24 02:15:58 +0300

    Changing traffic generation to be a smoother cyclic function + Protocol - TCP instead of UDP + Packet size - Normal distribution instead of constant + Add delay with sleep command so less active jobs are running at a time + Don't load localhost + Oscillate the load from the starting point + Some logging corrections

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -3,68 +3,70 @@ import argparse
 import logging
 import random
 import os
-from netaddr import iter_iprange
+from netaddr import iter_iprange, IPAddress
 
-baseCommand = "ITGSend -m rttm -a"
+baseCommand = "sleep %d ; ITGSend"
 
 # IPs
 firstHost = "10.0.0.1"
 lastHostTemplate = "10.0.0."
 
-expDurationMinutes = 200
-expDurationSeconds = expDurationMinutes*60
-expDurationMilliSeconds = 1000*expDurationSeconds
-
-periodLengthMilliSeconds = 240*1000
-periodSendLength = 220 * 1000
-periods = expDurationMilliSeconds/periodLengthMilliSeconds
-
-delayRandomness = {
-    "min": 0,
-    "max": 20000
-}
-
-# Packets per Second
+# Packets per Second range for Poisson distribution of inter-departure time
 dynamicPPS = {
-    "min": 200,  # ~20% of 10 Mbit
-    "max": 600  # ~60% of 10Mbit
+    "min": 330,  # ~20% of 10 Mbit for 600 byte mean
+    "max": 1000  # ~60% of 10Mbit for 600 byte mean
 }
-defaultPacketSize = 1024
+# Based roughly on http://www.caida.org/research/traffic-analysis/AIX/plen_hist/
+packetSizeMean = 600
+packetSizeStdDev = 100
 
 
-def create_command(base, dest, duration, delay, pps, size):
-    cmd = base
-    cmd += " %s"%dest
-    cmd += " -t %s"%duration
-    cmd += " -d %s"%delay
-    cmd += " -O %s"%pps
-    cmd += " -c %s"%size
+def create_command(base, dest, protocol, duration, delay_secs, pps, packet_size_mean, packet_size_std_dev):
+    cmd = base % delay_secs
+    cmd += " -a %s" % dest
+    cmd += " -T %s" % protocol
+    cmd += " -t %s" % duration
+    cmd += " -O %s" % pps
+    cmd += " -n %s %s" % (packet_size_mean, packet_size_std_dev)
     cmd += " < /dev/null &"
     return cmd
 
 
-def create_dynamic_loader_commands(hosts):
+def create_dynamic_loader_commands(hosts, protocol, periods, period_length_milliseconds, diff_abs):
     commands = []
+    pps = random.randrange(dynamicPPS["min"], dynamicPPS["max"])
+    diff = set_diff(pps, diff_abs)
     for i in range(periods):
-        delay = i * periodLengthMilliSeconds + random.randrange(delayRandomness["min"],delayRandomness["max"])
-        pps = random.randrange(dynamicPPS["min"],dynamicPPS["max"])
-        loaded_host = hosts[random.randrange(len(hosts))]
-        cmd = create_command(baseCommand, loaded_host, periodSendLength, delay ,pps, defaultPacketSize)
+        delay_secs = period_length_milliseconds / 1000
+        loaded_host = hosts[i % len(hosts)]
+        cmd = create_command(baseCommand,
+                             loaded_host,
+                             protocol,
+                             period_length_milliseconds,
+                             delay_secs,
+                             pps,
+                             packetSizeMean, packetSizeStdDev)
         commands.append(cmd)
+        pps += diff
+        if pps < dynamicPPS["min"] or pps > dynamicPPS["max"]:
+            diff = -diff
+            pps += 2*diff
     return commands
 
 
-def create_dynamic_loaders_commands(loaded_host, num_hosts):
-    commands = [[] for _ in range(num_hosts)]
-    for h in range(num_hosts):
-        commands[h].extend(create_dynamic_loader_commands(loaded_host))
-    return commands
+def set_diff(pps, diff_abs):
+    if (pps - dynamicPPS["min"]) / (dynamicPPS["max"] - dynamicPPS["min"]) > 0.5:
+        return diff_abs
+    else:
+        return -diff_abs
 
 
-def write_cmds_to_file(cmds, filename):
-    with open(filename,"w") as f:
+def write_commands_to_file(commands, filename):
+    logging.info("Writing to %s", filename)
+    with open(filename, "w") as f:
         f.write("#!/bin/bash"+os.linesep)
-        map(lambda c: f.write(c+os.linesep),cmds)
+        for c in commands:
+            f.write(c + os.linesep)
         f.write("wait"+os.linesep)
     # give it executable permissions
     st = os.stat(filename)
@@ -73,13 +75,13 @@ def write_cmds_to_file(cmds, filename):
 
 def validate_args(args):
     if os.path.isdir(args.config_dir):
-        logging.info("Creating config files in: " + args.config_dir)
+        logging.info("Creating config files in dir: %s", args.config_dir)
     else:
-        logging.fatal("Argument specified \"" + args.config_dir + "\" is not a directory!")
+        logging.fatal("Argument specified \"%s\" is not a directory!", args.config_dir)
         exit(1)
     if not 1 <= args.num_hosts <= 253:
         logging.fatal(
-            """The number of hosts specified \"{0}\" cannot be within one subnet!
+            """The number of hosts specified \"%d\" cannot be within one subnet!
 Should be {1..253}!""",
             args.num_hosts)
         exit(1)
@@ -87,10 +89,18 @@ Should be {1..253}!""",
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--config-dir",required=True,
+    parser.add_argument("-d", "--config-dir", required=True,
                         help="The configuration file directory (where the config files will be written to)")
+    parser.add_argument("--protocol", default="TCP",
+                        help="The transmission protocol to be used")
     parser.add_argument("-n", "--num-hosts", type=int, required=True,
                         help="The number of hosts to generate configuration files for")
+    parser.add_argument("-p", "--periods", type=int, default=3600,
+                        help="The number of experiment periods in the configuration files")
+    parser.add_argument("-l", "--period-length", type=int, default=10000,
+                        help="The length of an experiment period in milliseconds")
+    parser.add_argument("-a", "--absolute-pps-difference", type=int, default=10,
+                        help="The amount to increase/decrease the pps by per period")
     parser.add_argument("--debug", action="store_true", help="Set verbosity to high (debug level)")
     return parser.parse_args()
 
@@ -105,16 +115,26 @@ if __name__ == '__main__':
 
     validate_args(args)
 
+    logging.info("Generating with %d periods", args.periods)
+
     last_host = lastHostTemplate + str(args.num_hosts)
     ip_addresses = list(iter_iprange(firstHost, last_host, step=1))
 
     logging.info("Creating configuration for IPs:")
     logging.info(ip_addresses)
 
-    for l in ip_addresses:
-        cmds = create_dynamic_loader_commands(ip_addresses)
-        logging.info("Creating commands for " + str(l))
-        logging.debug(cmds)
-        write_cmds_to_file(cmds, args.config_dir+"/config-%s"%(l))
+    for address in ip_addresses:
+        address = str(address)
+        logging.info("Creating commands for " + address)
+        loaded_addresses = ip_addresses.copy()
+        # Remove localhost address
+        loaded_addresses.remove(IPAddress(address))
+        commands = create_dynamic_loader_commands(loaded_addresses,
+                                                  args.protocol,
+                                                  args.periods,
+                                                  args.period_length,
+                                                  args.absolute_pps_difference)
+        logging.debug(commands)
+        write_commands_to_file(commands, args.config_dir + "/config-" + address)
 
     logging.info("Done generating host config files!")
