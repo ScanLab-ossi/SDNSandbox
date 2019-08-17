b62eb9952a3376e59ae707b3c1e958b8a53c21d1
Yossi Solomon <yosisolomon@gmail.com>
2019-08-17 16:11:34 +0300

    Add proper input error messages

--- demo/util/show_drops_per_ip.sh
+++ demo/util/show_drops_per_ip.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-for i in $( seq $HOSTS_NUM )
+exit_with_msg() {
+  echo
+  echo $1
+  echo Exiting...
+  exit 1
+}
+
+if [ -z "$NUM_HOSTS" ] ; then
+  exit_with_msg "The variable NUM_HOSTS must be set to the number of switches/hosts used in the experiment!"
+fi
+
+if [ -z "$EXP_DIR" ] ; then
+  exit_with_msg "The variable EXP_DIR must be set to the name of the experiment directory name!"
+fi
+
+for i in $( seq $NUM_HOSTS )
 do
-	echo Drops for 10.0.0.$i = `grep "10.0.0.$i failed, skipping" *.log | wc -l`
+	echo Drops for 10.0.0.$i = `grep "10.0.0.$i failed, skipping" $EXP_DIR/*.log | wc -l`
 done
