7f8ba495330a013b544a7a608ed01b7ee6f575b4
Yossi Solomon <yosisolomon@gmail.com>
2020-09-08 17:17:44 +0300

    add printouts of ITGSend status code regardless of the code

--- scripts/generate_sender_scripts.py
+++ scripts/generate_sender_scripts.py
@@ -42,10 +42,12 @@ def get_ITGSend_cmd(multiflow_filename, duration_ms, grace_period, sleep_period)
           (int(grace_period + duration_ms / 1000), multiflow_filename)
     cmd = "echo \"Using following multiflow commands:\"; cat %s\n" \
           "date; %s; STATUS=$?\n" \
+          "date; echo \"ITGSend finished with status code = $STATUS\"\n" \
           "if [ ! $STATUS ]; then \n" \
           "echo \"ITGSend failed with status code $STATUS ... Trying again in %d secs!\"\n"\
           "sleep %d \n" \
           "date; %s; STATUS=$?\n" \
+          "date; echo \"ITGSend finished with status code = $STATUS\"\n" \
           "[ ! $STATUS ] && echo \"ITGSend failed again (with status code $STATUS )\"\nfi" %\
           (multiflow_filename, send_cmd, sleep_period, sleep_period, send_cmd)
     return cmd
