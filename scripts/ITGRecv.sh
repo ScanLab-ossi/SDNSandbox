8c8238c06f5afd7e8e88f960526c5fc7f5b8b298
Yossi Solomon <12303057+yossisolomon@users.noreply.github.com>
2020-10-01 23:54:16 +0300

    make ITGRecv log to receiver log file

--- scripts/ITGRecv.sh
+++ scripts/ITGRecv.sh
@@ -2,7 +2,7 @@
 # Tiny script that re-launches ITGRecv in case it dies...
 LOG_NAME=$EXP_DIR/receiver-$1.log 
 echo_this () {
-  echo [`date`] $1  >> $LOG_NAME 
+  echo [`date`] $1  >> $LOG_NAME
 }
 
 while [ 1 ]
@@ -10,7 +10,7 @@ do
 	echo_this "-------------------------------"
 	echo_this "Starting ITGRecv"
 
-	ITGRecv
+	ITGRecv &> $LOG_NAME
 
 	echo_this "ITGRecv has stopped!"
 	sleep 1
