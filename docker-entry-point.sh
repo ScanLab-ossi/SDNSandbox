fcbacb70b21c27cdfbdee06e220ce969ee796d48
Yossi Solomon <12303057+yossisolomon@users.noreply.github.com>
2020-11-09 23:44:13 +0200

    fix entrypoint PYTHONPATH

--- docker-entry-point.sh
+++ docker-entry-point.sh
@@ -2,4 +2,4 @@
 set -e
 
 service openvswitch-switch start
-PYTHONPATH=$PYTHONPATH;./mininet python3  -m sdnsandbox -c example.config.json -o /opt
+PYTHONPATH="./mininet" python3 -m sdnsandbox -c example.config.json -o /opt
