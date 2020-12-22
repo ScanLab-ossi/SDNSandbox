b62e7e2d80ed0de10393b9e2c6144176045f8885
Yossi Solomon <12303057+yossisolomon@users.noreply.github.com>
2020-12-22 02:07:34 +0200

    Adding the plotting post processor

--- sdnsandbox/processor.py
+++ sdnsandbox/processor.py
@@ -2,11 +2,16 @@ import json
 import logging
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from os import makedirs
 from typing import List, Any, Dict, Type, Optional
 import pandas as pd
 from scipy.stats import iqr
 from dacite import from_dict
 from os.path import join as pj
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +24,7 @@ class Processor(ABC):
 
 @dataclass
 class IQRProcessor(Processor):
-    iqr_filename: str
+    iqr_filename: str = 'iqr.json'
 
     def process(self, sampling_df: pd.DataFrame, output_path: str):
         results = self.get_iqr_results(sampling_df)
@@ -60,8 +65,30 @@ class IQRProcessor(Processor):
         return result
 
 
+@dataclass
+class PlottingProcessor(Processor):
+    plots_dirname: str = 'plots'
+
+    def process(self, sampling_df: pd.DataFrame, output_path: str):
+        plots_dir = pj(output_path, self.plots_dirname)
+        logger.info("Creating directory " + plots_dir + " for plots")
+        makedirs(plots_dir)
+        for column in sampling_df.keys():
+            logger.info("Plotting samples for port " + column)
+            sampling_df[column].plot()
+            plt.xlabel('Time')
+            plt.ylabel('MB/s')
+            plt.tight_layout()
+            filename = pj(plots_dir, "link_load_" + column + ".png")
+            logger.info("Saving plot to " + filename)
+            plt.savefig(filename)
+            plt.close()
+        logger.info("Done plotting @ " + plots_dir)
+
+
 class ProcessorsFactory:
-    types = {'IQR': IQRProcessor}  # type: Dict[str, Type[Processor]]
+    types = {'IQR': IQRProcessor,
+             'Plotting': PlottingProcessor}  # type: Dict[str, Type[Processor]]
 
     @classmethod
     def create(cls, processors_config: List[Dict[str, Any]], types: Optional[Dict[str, Type[Processor]]] = None)\
