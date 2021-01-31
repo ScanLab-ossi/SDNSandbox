a1eba3ed61cfdbf2a9dbeb93bd4923c11b3d5ed1
Yossi Solomon <yosisolomon@gmail.com>
2021-01-31 20:20:48 +0200

    Remove sudo usage from script

--- sdnsandbox/scripts/set_ovs_sflow.sh
+++ sdnsandbox/scripts/set_ovs_sflow.sh
@@ -2,7 +2,7 @@
 
 
 # a workaround to get all ovs switches
-_switch=`sudo ovs-vsctl list br | awk -- '/^name/ {print $3; exit;} '`
+_switch=`ovs-vsctl list br | awk -- '/^name/ {print $3; exit;} '`
 _switch=`echo $_switch | tr -d "\""`
 
 # analysis setting
@@ -11,15 +11,15 @@ agent_ip=eth0
 header_bytes=128
 sampling_n=1000000
 polling_sec=1
-RUN="sudo ovs-vsctl -- --id=@sflow create sflow \
-            agent=${agent_ip}           \
-            target=\"${collector_ip}\"  \
-            header=${header_bytes}      \
-            sampling=${sampling_n}      \
-            polling=${polling_sec}      \
-            -- set bridge ${_switch} sflow=@sflow"
+RUN="ovs-vsctl -- --id=@sflow create sflow \
+               agent=${agent_ip}           \
+               target=\"${collector_ip}\"  \
+               header=${header_bytes}      \
+               sampling=${sampling_n}      \
+               polling=${polling_sec}      \
+               -- set bridge ${_switch} sflow=@sflow"
 
-for br in `sudo ovs-vsctl list br | awk -- '/^name/ {print $3;} '`; do
+for br in `ovs-vsctl list br | awk -- '/^name/ {print $3;} '`; do
     br=`echo $br | tr -d "\""`
     RUN="$RUN -- -- set bridge $br sflow=@sflow"
 done
